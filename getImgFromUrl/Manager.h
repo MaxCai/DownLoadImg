aa998ed3f86298defc91fb351b8e10a13bbf5188
MaxCai <caiye1231@163.com>
2014-07-13 21:23:36 +0800

    download the images which are recommanded.

--- getImgFromUrl/Manager.h
+++ getImgFromUrl/Manager.h
@@ -5,6 +5,7 @@
 #include <QNetworkAccessManager>
 
 class QNetworkReply;
+#include <QStringList>
 
 class ImgGetter : public QObject
 {
@@ -12,23 +13,45 @@ class ImgGetter : public QObject
 public:
 	ImgGetter(QObject *parent = 0)
 		:QObject(parent),
-		m_pNetManager(new QNetworkAccessManager(this))
+                m_pNetManager(new QNetworkAccessManager(this)),
+                state(GET_HOME)
 	{
 		connect(m_pNetManager, SIGNAL(finished(QNetworkReply *)), this, SLOT(replyFinished(QNetworkReply *)));
-	}
+
+                connect(this, SIGNAL(hrefGot()), this, SLOT(getHref()));
+                connect(this, SIGNAL(imgGot()), this, SLOT(getImg()));
+            }
 	~ImgGetter(){}
 
-	void get(char *url);
+        void get(char *url);
 private:
-	void getImgUrl(QNetworkReply *reply);
+        void getImgUrl(const QString &string);
 	void loadImg(QNetworkReply *reply);
 
+        void searchPixmapHome(const QString &string);
+
+        void searchImg(QNetworkReply *reply);
+signals:
+        void hrefGot();
+        void imgGot();
 public slots:
 	void replyFinished(QNetworkReply *);
+        void getHref();
+        void getImg();
 
 private:
 	QNetworkAccessManager *m_pNetManager;
-	QString m_imgUrl;
+        QStringList m_imgHref;
+        QStringList m_imgUrlList;
+
+        int state;
+
+        enum
+        {
+            GET_HOME,
+            GET_IMG_HREF,
+            GET_LOAD_IMG
+        };
 };
 
 #endif
